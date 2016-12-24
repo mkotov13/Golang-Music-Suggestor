907ec753fc3d1a75e8e0a6e735a6f2ab817f30c8
mkotov13 <mkotov13@simons-rock.edu>
2016-12-24 04:29:58 -0500

    now able to understand flags

--- src/mnm/mnm.go
+++ src/mnm/mnm.go
@@ -8,13 +8,32 @@ import (
 )
 
 func main()  {
+  var genre string
 
   app := cli.NewApp()
+
   app.Name = "M&M"
   app.Usage = "one day we will build an actually impressive Go script"
+
+  app.Flags = []cli.Flag {
+    cli.StringFlag{
+      Name: "genre, g",
+      Value: "future bass",
+      Usage: "deciding what music to offer",
+      Destination: &genre,
+    },
+  }
+
   app.Action = func (c *cli.Context) error {
-    fmt.Println("Hi! Welcome to the M&M commandline app\n")
-    fmt.Println("It was created by Max and Michael.\nWe couldn't really decide who's name goes first in the title, hence M&M")
+    fmt.Println("\nHi! Welcome to the M&M commandline app\n")
+    fmt.Println("This is not your simple cli app - it can understand flags, too.\nBased on user input, the app returns a song of a certain genre.\n")
+    fmt.Printf("Listen to ")
+
+    if genre == "rap" {
+      fmt.Printf("Kanye West - New Slaves")
+    } else {
+      fmt.Printf("Marhsmello - Alone")
+    }
 
     return nil
   }
